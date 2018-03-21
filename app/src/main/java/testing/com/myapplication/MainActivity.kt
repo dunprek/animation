bdcec032ab66d9f89d1aff380b015ce35faf4765
GIDEON\don <gideon.tobing@hotmail.com>
2018-03-22 06:43:49 +0700

    change color on click

--- app/src/main/java/testing/com/myapplication/MainActivity.kt
+++ app/src/main/java/testing/com/myapplication/MainActivity.kt
@@ -5,21 +5,36 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.animation.AnimationUtils
+import android.widget.TextView
 import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : BaseActivity() {
-
-
-
+class MainActivity : BaseActivity(), View.OnClickListener {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+
+        val tvBlackWhite = findViewById<TextView>(R.id.tv_black_and_white)
+        tvBlackWhite.setOnClickListener(this)
+
+        val tvGraphicArt = findViewById<TextView>(R.id.tv_graphic_art)
+        tvGraphicArt.setOnClickListener(this)
+
+        val tvCopyCenter = findViewById<TextView>(R.id.tv_copy_center)
+        tvCopyCenter.setOnClickListener(this)
+
+        tvBlackWhite.setSelected(false);
+        tvBlackWhite.setPressed(false);
+        tvGraphicArt.setSelected(false);
+        tvGraphicArt.setPressed(false);
+        tvCopyCenter.setSelected(false);
+        tvCopyCenter.setPressed(false);
 
         // slide-up animation
     }
@@ -63,6 +78,88 @@ class MainActivity : BaseActivity() {
     private fun toast(message: CharSequence) =
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
+    override fun onClick(view: View) {
+        // clear state
+
+
+        when (view.id) {
+            R.id.tv_black_and_white -> {
+                if(tv_black_and_white.isSelected){
+                    tv_black_and_white.isSelected = false
+                    tv_black_and_white.setTextColor(resources.getColor(R.color.colorTransparent))
+
+                }
+                else{
+                    tv_black_and_white.isSelected = true
+                    tv_black_and_white.setTextColor(resources.getColor(R.color.colorWhite))
+                    toast("tv_black_and_white")
+                }
+            }
+/////////////////////////////////////////////////////////////
+            R.id.tv_colored -> {
+                if(tv_colored.isSelected){
+                    tv_colored.isSelected = false
+                    tv_colored.setTextColor(resources.getColor(R.color.colorTransparent))
+
+                }
+                else{
+                    tv_colored.isSelected = true
+                    tv_colored.setTextColor(resources.getColor(R.color.colorWhite))
+
+                    toast("tv_colored")
+                }
+            }
+//////////////////////////////////////////////////////////////
+            R.id.tv_graphic_art -> {
+                if(tv_graphic_art.isSelected){
+                    tv_graphic_art.isSelected = false
+                    tv_graphic_art.setTextColor(resources.getColor(R.color.colorTransparent))
+
+                }
+                else{
+                    tv_graphic_art.isSelected = true
+                    tv_graphic_art.setTextColor(resources.getColor(R.color.colorWhite))
+
+                    toast("tv_graphic_art")
+
+                }
+
+            }
+
+/////////////////////////////////////////////////////////////
+            R.id.tv_copy_center -> {
+                if(tv_copy_center.isSelected){
+                    tv_copy_center.isSelected = false
+                    tv_copy_center.setTextColor(resources.getColor(R.color.colorTransparent))
+
+                }
+                else{
+                    tv_copy_center.isSelected = true
+                    tv_copy_center.setTextColor(resources.getColor(R.color.colorWhite))
+
+                    toast("copy center")
+
+                }
+            }
+////////////////////////////////////////////////////////////
+            R.id.tv_office_printer -> {
+                if(tv_office_printer.isSelected){
+                    tv_office_printer.isSelected = false
+                    tv_office_printer.setTextColor(resources.getColor(R.color.colorTransparent))
+
+                }
+                else{
+                    tv_office_printer.isSelected = true
+                    tv_office_printer.setTextColor(resources.getColor(R.color.colorWhite))
+                    toast("office printer")
+                }
+            }
+
+        }
+    }
+
+
 }
+
 
 
